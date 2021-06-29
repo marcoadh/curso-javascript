6ff45f62e21febee1bc5eef2f5cb30a6060b35cd
marcodlc <delacruzhenriquez.marco@gmail.com>
2021-06-29 10:33:11 -0500

    DOM: Traversing

---
+++ js/dom/dom-traversing.js
@@ -0,0 +1,11 @@
+const $menu = document.getElementById("menu");
+
+console.log($menu);
+console.log($menu.children);
+console.log($menu.children[2]);
+console.log($menu.parentElement);
+console.log($menu.firstElementChild);
+console.log($menu.lastElementChild);
+console.log($menu.previousElementSibling);
+console.log($menu.nextElementSibling);
+console.log($menu.children[1].closest("body"));
