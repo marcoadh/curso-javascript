aa15b4c5f83f05e35e6d1c01c10cbcb0e213e372
marcodlc <delacruzhenriquez.marco@gmail.com>
2021-07-03 09:37:25 -0500

    BOM: URL, Historial y Navegador

---
+++ js/bom/objetos.js
@@ -0,0 +1,35 @@
+// Objetos: URL, Historial y Navegador
+
+// console.log("-------- Objeto URL (location) --------");
+// console.log(location);
+// console.log(location.origin);
+// console.log(location.protocol);
+// console.log(location.host);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.href);
+// console.log(location.hash);
+// console.log(location.search);
+// console.log(location.pathname);
+
+// Funciona en base al historial de navegación
+
+// console.log("-------- Objeto Historial (history) --------");
+// console.log(history);
+// console.log(history.length);
+// history.forward(1);
+// history.back(2);
+// history.go(-3);
+
+
+console.log("-------- Objeto Navegador (navigator) --------");
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
