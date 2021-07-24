26f3752328ae6fc8b9003cadfff164c91b487827
marcodlc <delacruzhenriquez.marco@gmail.com>
2021-07-23 19:20:42 -0500

    Async-Await Part2

--- js/asincronia/async-await.js
+++ js/asincronia/async-await.js
@@ -15,54 +15,68 @@ function cuadradoPromesa(valor) {
 
 async function funcionAsincronaDeclarada() {
     try {
-        console.log("Inicio de Async Function.");
+        console.log("Inicio de Async Función Declarada.");
         let obj = await cuadradoPromesa(0);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(1);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(2);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
 
-        obj = await cuadradoPromesa("3");
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        obj = await cuadradoPromesa(3);
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(4);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(5);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
-        console.log("Fin de Async Function.")
+        console.log(`Async Function D: ${obj.valor}, ${obj.resultado}`);
+        console.log("Fin de Async Función Declarada.")
     } catch (err) {
         console.error(err);
     }
 }
 
-funcionAsincronaDeclarada();
+// funcionAsincronaDeclarada();
 
 
 const funcionAsincronaExpresada = async () => {
     try {
-        console.log("Inicio de Async Function.");
+        console.log("Inicio de Async Función Expresada.");
         let obj = await cuadradoPromesa(6);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function E: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(7);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function E: ${obj.valor}, ${obj.resultado}`);
 
-        obj = await cuadradoPromesa("8");
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        obj = await cuadradoPromesa(8);
+        console.log(`Async Function E: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(9);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
+        console.log(`Async Function E: ${obj.valor}, ${obj.resultado}`);
 
         obj = await cuadradoPromesa(10);
-        console.log(`Async Function: ${obj.valor}, ${obj.resultado}`);
-        console.log("Fin de Async Function.")
+        console.log(`Async Function E: ${obj.valor}, ${obj.resultado}`);
+        console.log("Fin de Async Función Expresada.")
     } catch (err) {
         console.error(err);
     }
 }
 
-funcionAsincronaExpresada();
+// funcionAsincronaExpresada();
+
+const funcionConPromesaAwaitTimeout = async () => {
+    console.log("Iniciando función Asíncrona");
+    let miPromesa = new Promise(resolver => {
+        setTimeout(() => {
+            resolver(`Promesa con await y setTimeout`)
+        }, 3000);
+    });
+    
+    console.log(await miPromesa);
+    console.log("Finalizando función Asíncrona");
+}
+
+// funcionConPromesaAwaitTimeout();
